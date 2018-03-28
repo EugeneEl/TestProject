0bb9474844bbf07b3c64834c168002b165c2dad7
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2018-03-28 18:48:23 +0300

    added user session model

---
+++ TestProject/TestProject/Models/UserSessionModel/UserSessionModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserSessionModel.swift
+//  TestProject
+//
+//  Created by Eugene Goloboyar on 28.03.2018.
+//  Copyright © 2018 Eugene Goloboyar. All rights reserved.
+//
+
+import Foundation
+
+struct UserSessionModel {
+    let userName: String
+    let token: String
+}
---
+++ TestProject/TestProject/Services/UserSessionService/UserSessionService.swift
@@ -0,0 +1,23 @@
+//
+//  UserSessionService.swift
+//  TestProject
+//
+//  Created by Eugene Goloboyar on 28.03.2018.
+//  Copyright © 2018 Eugene Goloboyar. All rights reserved.
+//
+
+import Foundation
+
+final class UserSessionService {
+    
+    // MARK: - Vars
+    
+    static let shared = UserSessionService()
+    
+    var isLogges: Bool
+    fileprivate (set) internal var sessionModel: UserSessionModel?
+    
+    // MARK: - Initialization
+    
+    private init() {}
+}
