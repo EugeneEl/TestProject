f877d8e789c1d1243364541fd9d0d95c20a2ec94
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2018-03-28 18:40:25 +0300

    base project setup

---
+++ TestProject/TestProject/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestProject
+//
+//  Created by Eugene Goloboyar on 28.03.2018.
+//  Copyright © 2018 Eugene Goloboyar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TestProject/TestProject/Scenes/RootRouter.swift
@@ -0,0 +1,9 @@
+//
+//  RootRouter.swift
+//  TestProject
+//
+//  Created by Eugene Goloboyar on 28.03.2018.
+//  Copyright © 2018 Eugene Goloboyar. All rights reserved.
+//
+
+import Foundation
