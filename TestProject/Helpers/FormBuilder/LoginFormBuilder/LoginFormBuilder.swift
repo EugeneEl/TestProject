08263066a71a6deadc4b108cf3f91eac7ac0b534
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2018-08-19 11:11:47 +0300

    login form builder base code

--- TestProject/Helpers/FormBuilder/LoginFormBuilder/LoginFormBuilder.swift
+++ TestProject/Helpers/FormBuilder/LoginFormBuilder/LoginFormBuilder.swift
@@ -7,81 +7,54 @@
 //
 
 import Foundation
+import UIKit
 
-/// Fragment types for composing Settings Main Screen.
-enum SettingsMainFragmentType {
-    case myInfo
-    case support
-    case notification
-    case location
-    case logout
+/// Form types for composing Login Screen.
+enum LoginFormType {
+    case email
+    case password
     
-    var height: CGFloat {
+    var formUI: FormInputViewUI {
         switch self {
-        case .myInfo, .support:
-            return 44
-        case .location, .notification:
-            return 70
-        case .logout:
-            return 47
+        case .email:
+            return FormInputViewUI(placeholderText: "Email:",
+                                   formKeyboardUI: keyboardUI,
+                                   textFont: UIFont.systemFont(ofSize: 15),
+                                   placeholderRightPadding: 8)
+        case .password:
+            return FormInputViewUI(placeholderText: "Password:",
+                                   formKeyboardUI: keyboardUI,
+                                   textFont: UIFont.systemFont(ofSize: 15),
+                                   placeholderRightPadding: 8)
+        }
+    }
+    
+    var keyboardUI: FormKeyboardUI {
+        switch self {
+        case .email:
+            return FormKeyboardUI(keyboardType: .emailAddress,
+                                  capitalizationType: .none,
+                                  autocorrectionType: .no)
+        case .password:
+            return FormKeyboardUI(keyboardType: .default,
+                                  capitalizationType: .none,
+                                  autocorrectionType: .no)
         }
     }
 }
 
 final class LoginFormBuilder {
     
-    /// Provide an input control and view for settings fragment type.
+    /// Provide an input control and view for login screen.
     ///
     /// - Parameters:
-    ///   - fragment: `SettingsMainFragmentType` object.
-    ///   - viewController: `UIViewController` instance to pass in fragment (required for navigation for specific fragment).
-    /// - Returns: A tuple with `MainProfileControl` inputControl (contains input and validation logic) and `UIView` instance for fragment.
-    static func provideInputControlModuleForFragment(_ fragment: SettingsMainFragmentType, viewController: UIViewController) -> (MainProfileControl, UIView) {
-        switch fragment {
-        case .myInfo:
-            let infoView = SettingsInfoView.instantiateView()
-            infoView.configureWithText("geo_report_myinfo_title".localized)
-            let inputControl = SettingsInfoInputControl(settingsInfoView: infoView, viewController: viewController, type: fragment)
-            infoView.delegate = inputControl
-            
-            return (inputControl, infoView)
-        case .support:
-            let infoView = SettingsInfoView.instantiateView()
-            infoView.configureWithText("profile_settings_support_hint".localized, isImageHidden: false)
-            let inputControl = SettingsInfoInputControl(settingsInfoView: infoView, viewController: viewController, type: fragment)
-            
-            return (inputControl, infoView)
-        case .notification:
-            let notificationPermissionsView = SettingsPermissionInputView.instantiateView()
-            let inputControl = PushSettingsPermissionInputControl(notificationView: notificationPermissionsView)
-            notificationPermissionsView.delegate = inputControl
-            inputControl.configureNotificationView()
-            
-            let topUI = SettingsSeparatorUI(isVisible: true, color: UIColor(hex:"C8C7CC"), hasLeftPadding: false)
-            let bottomUI = SettingsSeparatorUI(isVisible: true, color: UIColor.black.withAlphaComponent(0.12), hasLeftPadding: true)
-            
-            notificationPermissionsView.configureSeparators(topUI: topUI, bottomUI: bottomUI)
-            
-            return (inputControl, notificationPermissionsView)
-        case .location:
-            let notificationPermissionsView = SettingsPermissionInputView.instantiateView()
-            let inputControl = LocationSettingsPermissionInputControl(notificationView: notificationPermissionsView)
-            notificationPermissionsView.delegate = inputControl
-            inputControl.configureNotificationView()
-            
-            let topUI = SettingsSeparatorUI(isVisible: false, color: UIColor(hex:"C8C7CC"), hasLeftPadding: false)
-            let bottomUI = SettingsSeparatorUI(isVisible: true, color: UIColor(hex:"C8C7CC"), hasLeftPadding: false)
-            
-            notificationPermissionsView.configureSeparators(topUI: topUI, bottomUI: bottomUI)
-            
-            return (inputControl, notificationPermissionsView)
-        case .logout:
-            let logoutView = SettingsLogoutView.instantiateView()
-            
-            let inputControl = SettingsLogoutInputControl(viewController: viewController)
-            logoutView.delegate = inputControl
-            
-            return (inputControl, logoutView)
-        }
+    ///   - formType: `LoginFormType` type.
+    /// - Returns: A tuple with `FormInputConroller` inputControl (contains input logic) and `UIView` instance for form.
+    static func provideInputControlModuleForFragment(_ formType: LoginFormType) -> (FormInputConroller, UIView) {
+        
+        let view = FormInputView.instantiateView()
+        let inputControl = FormInputConroller(formConfigurating: view, formInputViewUI: formType.formUI)
+        
+        return (inputControl, view)
     }
 }
