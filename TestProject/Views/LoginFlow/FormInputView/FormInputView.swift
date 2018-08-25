18c3d713c3ff6108504d4c8859c63da1de087f08
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2018-08-25 09:43:53 +0300

    moved ui for forms to separate file

--- TestProject/Views/LoginFlow/FormInputView/FormInputView.swift
+++ TestProject/Views/LoginFlow/FormInputView/FormInputView.swift
@@ -9,41 +9,6 @@
 import Foundation
 import UIKit
 
-enum FormInputRightViewMode {
-    case showPassword(Bool)
-    
-    var image: UIImage {
-        switch self {
-        case .showPassword(let isVisible):
-            if isVisible {
-                return #imageLiteral(resourceName: "showPassword")
-            } else {
-                return #imageLiteral(resourceName: "hidePassword")
-            }
-        }
-    }
-}
-struct FormKeyboardUI {
-    let keyboardType: UIKeyboardType
-    let capitalizationType: UITextAutocapitalizationType
-    let autocorrectionType: UITextAutocorrectionType
-    let isSecureTextEntry: Bool
-}
-
-struct FormPlaceholderUI {
-    let text: String
-    let rightPadding: CGFloat
-    let leftPadding: CGFloat
-    let width: CGFloat
-}
-
-struct FormInputViewUI {
-    let placeholderUI: FormPlaceholderUI
-    let keyboardUI: FormKeyboardUI
-    let textFont: UIFont
-    let isSeparatorVisible: Bool
-}
-
 protocol FormInputViewConfigurating: class {
     func setupFormInputViewWithFormUI(_ formUI: FormInputViewUI, delegate: UITextFieldDelegate)
 }
