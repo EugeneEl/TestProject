ddfda942ccb184d8969f93783dc7e6bc088fa550
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2018-08-25 16:37:35 +0300

    base list ui

--- TestProject/Views/MainFlow/FeedListTableViewCell/FeedListTableViewCell.swift
+++ TestProject/Views/MainFlow/FeedListTableViewCell/FeedListTableViewCell.swift
@@ -17,6 +17,7 @@ class FeedListTableViewCell: UITableViewCell {
     
     // MARK: - Outlets
     
+    @IBOutlet fileprivate weak var containerView: UIView!
     @IBOutlet fileprivate weak var titleLabel: UILabel!
     @IBOutlet fileprivate weak var dateLabel: UILabel!
     @IBOutlet fileprivate weak var summaryLabel: UILabel!
@@ -25,6 +26,9 @@ class FeedListTableViewCell: UITableViewCell {
     // MARK: - Vars
     
     weak var delegate: FeedListTableViewCellInteractable?
+    fileprivate static let shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+    fileprivate static let shadowRadius: CGFloat = 4
+    fileprivate static let shadowOffset: CGSize = CGSize(width: 0, height: 2)
     
     // MARK: - Actions
     
@@ -44,5 +48,13 @@ extension FeedListTableViewCell: DataObjectConfigurable {
         print("object.summary: \(object.summary)")
         linkLabel.text = object.url.absoluteString
         dateLabel.text = FeedListCellPresenter.provideDateTextForFeedItem(object)
+    }
+    
+    fileprivate func addShadow() {
+        containerView.layer.shadowColor = FeedListTableViewCell.shadowColor
+        containerView.layer.shadowRadius = FeedListTableViewCell.shadowRadius
+        containerView.layer.shadowOffset = FeedListTableViewCell.shadowOffset
+        containerView.layer.shadowOpacity = 1
+        containerView.layer.masksToBounds = false
     }
 }
